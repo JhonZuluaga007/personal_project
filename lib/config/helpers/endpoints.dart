19b96867a8427110488467e1133ca6bd75c847b8
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2023-01-28 09:53:38 -0500

    Added dependency injection for password reset use case

--- lib/config/helpers/endpoints.dart
+++ lib/config/helpers/endpoints.dart
@@ -3,6 +3,11 @@ import 'package:personal_project/config/config.dart';
 class Endpoints {
   //  Endpoint para autenticarse (Login). Acepta POST.
   static const String login = '${Config.baseURL}login';
-
   static const String getUser = '${Config.baseURL}users/';
+  static const String resetPassword = '${Config.baseURL}users/reset-password';
+  static const String changePassword = '${Config.baseURL}users/reset';
+  //  Endpoint para editMedicalHistory (EditMedicalHistory). Acepta POST.
+  static const String editMedicalHistory = '${Config.baseURL}formmedical/';
+  //  Endpoint para viewMedicalHistory (ViewMedicalHistory). Acepta GET.
+  static const String viewMedicalHistory = '${Config.baseURL}formmedical/list/';
 }
