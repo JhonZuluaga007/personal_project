91dac219b33f9e923762c2d06bf8f09d096b9bda
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2023-02-06 21:29:06 -0500

    update services validate antigen

--- lib/features/antigen/data/data_source/antigen_data_source.dart
+++ lib/features/antigen/data/data_source/antigen_data_source.dart
@@ -1,18 +1,19 @@
 import 'package:personal_project/config/helpers/api.dart';
 import 'package:personal_project/config/helpers/endpoints.dart';
+import 'package:http/http.dart' as http;
 
 class AntigenDataSource {
-  Future<bool> validateAntigen(String userId, String code) async {
-    final response = await Api.post(
-        Endpoints.validateAntigen, {'code': code, 'userId': userId});
+  void validateAntigen(String userId, String code) async {
+    var request =
+        http.MultipartRequest('POST', Uri.parse(Endpoints.validateAntigen));
+    request.fields.addAll({'code': code, 'userId': userId});
 
-    if (response["statusCode"] == 200) {
-      print('esooo hp');
-      return true;
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
     } else {
-      print('GONO hp');
-
-      return false;
+      print(response.reasonPhrase);
     }
   }
 }
