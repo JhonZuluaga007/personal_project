bb66aaab4d496374d3f112c808048f647613ada8
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2023-02-11 15:43:18 -0500

    [Feature/Update] the flow of the call of the test view is eliminated

--- lib/features/test_history/data/data_source/test_history_data_source.dart
+++ lib/features/test_history/data/data_source/test_history_data_source.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../models/test_view_model.dart';
 import '../models/test_history_models.dart';
 import '../../../../config/helpers/api.dart';
 import '../../../../config/helpers/endpoints.dart';
-import '../../domain/entities/test_view_entity.dart';
 import '../../domain/entities/test_history_entity.dart';
 import '../../../../config/helpers/errors/invalid_data.dart';
 
@@ -17,22 +15,6 @@ class TestHistoryDataSource {
         debugPrint(
             "result data response historyTest.data.length: ${historyTest.data.length}");
         return historyTest.data;
-      } else {
-        throw InvalidData("No se encontro datos");
-      }
-    } on InvalidData catch (invalidData) {
-      throw InvalidData("Data no encontrada");
-    }
-  }
-
-  Future<List<TestViewEntity>> getViewTest(String testId) async {
-    try {
-      final response = await Api.get('${Endpoints.viewTest}$testId');
-      if (response["statusCode"] == 200) {
-        TestViewModel viewTest = TestViewModel.fromJson(response);
-        debugPrint(
-            "result data response historyTest.data.length: ${viewTest.data.length}");
-        return viewTest.data;
       } else {
         throw InvalidData("No se encontro datos");
       }
--- lib/features/test_history/data/repository/test_history_repository_impl.dart
+++ lib/features/test_history/data/repository/test_history_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'package:either_dart/either.dart';
 
-import '../../domain/entities/test_view_entity.dart';
 import '../data_source/test_history_data_source.dart';
 import '../../domain/entities/test_history_entity.dart';
 import '../../../../config/helpers/errors/invalid_data.dart';
@@ -18,18 +17,6 @@ class TestHistoryRepositoryImpl extends TestHistoryRepository {
     try {
       final List<TestHistoryEntity> response =
           await testHistoryDataSource.getHistoryTest(userId);
-      return Right(response);
-    } on InvalidData catch (invalidData) {
-      return Left(invalidData);
-    }
-  }
-
-  @override
-  Future<Either<InvalidData, List<TestViewEntity>>> getViewTest(
-      String testId) async {
-    try {
-      final List<TestViewEntity> response =
-          await testHistoryDataSource.getViewTest(testId);
       return Right(response);
     } on InvalidData catch (invalidData) {
       return Left(invalidData);
