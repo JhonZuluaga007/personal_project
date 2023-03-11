7f9e4c3e55306366c80db553a0d16a402b0873d9
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2023-03-11 12:15:51 -0500

    [Feature/Update] Update endpoints

--- lib/config/helpers/endpoints.dart
+++ lib/config/helpers/endpoints.dart
@@ -3,17 +3,22 @@ import 'package:Tellme/config/config.dart';
 class Endpoints {
   //  Endpoint para autenticarse (Login). Acepta POST.
   static const String login = '${Config.baseURL}auth/login';
+  
   static const String getUser =
       '${Config.baseURL}profile?project=ChelseaProject';
+  
   static const String resetPassword = '${Config.baseURL}auth/reset-password';
+
   static const String changePassword =
       '${Config.baseURL}profile/change-password';
   //  Endpoint para editMedicalHistory (EditMedicalHistory). Acepta POST.
   static const String editMedicalHistory = '${Config.baseURL}formmedical/';
   //  Endpoint para viewMedicalHistory (ViewMedicalHistory). Acepta GET.
   static const String viewMedicalHistory = '${Config.baseURL}formmedical/list/';
+  
   static const String testHistory =
       '${Config.baseURL}profile/medical-history?project=ChelseaProject';
+  
   static const String viewTest = '${Config.baseURL}tests/detail/';
 
 //Validate antigen Endpoint
@@ -21,7 +26,8 @@ class Endpoints {
 
 //Validate pcr endpoint
   static const String validatePcr = '${Config.baseURL}codepcrvalidator';
+  
   static const String registerTest = "${Config.baseURL}forms";
 
-  static const String testools = '${Config.baseURL}testools';
+  static const String optionsTools = '${Config.baseURL}options?project=ChelseaProject';
 }
